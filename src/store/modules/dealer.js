aba3f72cd2113bf7b73db6eaae42718dae2e169c
shangwenwu <shangwenwu@lionbridgecapital.cn>
2019-12-05 17:16:45 +0800

    update

--- src/store/modules/dealer.js
+++ src/store/modules/dealer.js
@@ -13,7 +13,7 @@ export default {
       "followStatus": 0,          // 跟进状态（0 全部 1 线索入库  2 合作中  3  备案准入 4 达成合作   5 放弃）
       "followerUserGids": [],     // 跟进人gid 集合
       "level": 0,                 // 经销商分级（1 一级 2 二级）
-      "notVisitDays": 0,          // 没有拜访的天数
+      "notVisitDays": '',          // 没有拜访的天数
       "orderType": 0,             // 排序规则：1 按拜访时间 2 按创建时间 3 按名称
       "ownerUserGids": [],        // 负责人gid 集合
       "pageNum": 1,               // 当前页数 1开始
@@ -94,7 +94,7 @@ export default {
         "followStatus": 0,          // 跟进状态（0 全部 1 线索入库  2 合作中  3  备案准入 4 达成合作   5 放弃）
         "followerUserGids": [],     // 跟进人gid 集合
         "level": 0,                 // 经销商分级（1 一级 2 二级）
-        "notVisitDays": 0,          // 没有拜访的天数
+        "notVisitDays": '',          // 没有拜访的天数
         "orderType": 0,             // 排序规则：1 按拜访时间 2 按创建时间 3 按名称
         "ownerUserGids": [],        // 负责人gid 集合
         "pageNum": 1,               // 当前页数 1开始
