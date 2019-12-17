f59ae880a80da9dbea1aa7c41ffba6f266c77119
shangwenwu <shangwenwu@lionbridgecapital.cn>
2019-12-17 19:51:58 +0800

    update

--- src/plugins/serveConf.js
+++ src/plugins/serveConf.js
@@ -1,6 +1,7 @@
-window.ENV = 'testServer'
+window.ENV = 'testServer' ///
 window.developServer = 'http://10.20.7.129:8000/'
 window.testServer = 'http://10.20.6.34:8000/'
+window.productionServer = 'sdsd'
 window.demoServer = 'http://10.20.5.22:8000/'
 window.picServer = 'http://10.20.7.115/'
 
