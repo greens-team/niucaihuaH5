4d700d62f0e525c2b1d5300556ac9dff8889cdd8
shangwenwu <shangwenwu@lionbridgecapital.cn>
2019-12-24 14:46:33 +0800

    update

--- src/plugins/serveConf.js
+++ src/plugins/serveConf.js
@@ -1,4 +1,4 @@
-window.ENV = 'testServer' ///
+window.ENV = 'productionServer' ///
 window.developServer = 'http://10.20.7.129:8000/lbccrm/'
 window.testServer = 'http://10.20.6.34:8000/lbccrm/'
 window.productionServer = 'https://lbccrm.shiqiaokache.com/'
