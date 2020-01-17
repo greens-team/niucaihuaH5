85df49b24c02b376089f38ecd4eebbabd7109be8
shangwenwu <shangwenwu@lionbridgecapital.cn>
2020-01-17 17:49:09 +0800

    update

--- src/plugins/serveConf.js
+++ src/plugins/serveConf.js
@@ -2,6 +2,6 @@ window.ENV = 'testServer' ///
 window.developServer = 'http://10.20.7.129:8000/lbccrm/'
 window.testServer = 'http://10.20.6.34:8000/lbccrm/'
 window.productionServer = 'https://lbccrm.shiqiaokache.com/lbccrm/'
-window.demoServer = 'http://10.20.5.22:8000/'
+window.demoServer = 'https://lbccrmh5.ekxkl.com.cn/lbccrm/'
 window.picServer = 'http://10.20.7.115/'
 
